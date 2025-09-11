490388fe487eb52fe15d764802c9edbf5d71e090
DSM3 <fatec-dsm3@academico.intra>
2025-09-10 22:11:19 -0300

    @catypr

--- 05-autenticacao-com-jsonwebtoken/middleware/Auth.js
+++ 05-autenticacao-com-jsonwebtoken/middleware/Auth.js
@@ -5,9 +5,25 @@ import userController from "../controllers/userController.js";
 const Authorization = (req, res, next) => {
     const authToken = req.headers['authorization']
     if (authToken != undefined) {
-        next()
+        // Dividindo a string do token(para eliminar a paralavra Bearer)
+        const bearer = authToken.split(" ")
+        const token = bearer[1]
+         // Validando o Token
+        jwt.verify(token, userController.JWTSecret, (error, data) => {
+            if (error) {
+                res.status(401).json({ error: "Token inválido" })
+                // Token Válido
+            }else {
+                req.token = token;
+                req.loggedUser = {
+                    id: data.id,
+                    email: data.email,
+                };
+                next();
+            }
+        })
     }else {
-        res.status(401).json({ error: "Token inválido"});
+        res.status(401).json({ error: "Acesso não autorizado"});
     }
 };
 export default {Authorization };
